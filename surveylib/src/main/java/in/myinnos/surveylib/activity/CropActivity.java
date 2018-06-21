bd28204ac11d157523fa5470fdb202cf84a0bc19
MyInnos <prabhakar_thota@live.com>
2018-06-21 17:26:52 +0530

    Fixed bugs

--- surveylib/src/main/java/in/myinnos/surveylib/activity/CropActivity.java
+++ surveylib/src/main/java/in/myinnos/surveylib/activity/CropActivity.java
@@ -127,7 +127,7 @@ public class CropActivity extends AppCompatActivity {
             startCameraIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             startCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
             startCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(getApplicationContext(),
-                    "com.rubanbridge.associate.provider", tempFile));
+                    "in.myinnos.surveylib.provider", tempFile));
 
             if (startCameraIntent.resolveActivity(getPackageManager()) != null) {
                 startActivityForResult(startCameraIntent, REQUEST_CODE_TAKE_PHOTO);
